0a3ec58c61dcb499ab9c5ed59cada3453baa9214
Ankit Kumar <ankitkr2731@gmail.com>
2024-09-20 03:59:06 +0530

    fix: proxy added

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    server: {
+        proxy: {
+          '/api': {
+            target: 'http://13.201.49.61:8020',
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/api/, '')
+          }
+        }
+      }
 });
